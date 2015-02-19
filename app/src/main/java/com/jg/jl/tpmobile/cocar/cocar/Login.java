4f145907af4fbdc5bf1fcf7dc8253ce1c80c37e0
JonathanLavoie <jonathan_lavoie_01@hotmail.com>
2015-02-19 13:14:51 -0500

    Interface Inscription

--- app/src/main/java/com/jg/jl/tpmobile/cocar/cocar/Login.java
+++ app/src/main/java/com/jg/jl/tpmobile/cocar/cocar/Login.java
@@ -1,9 +1,12 @@
 package com.jg.jl.tpmobile.cocar.cocar;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 
 public class Login extends ActionBarActivity {
@@ -12,5 +15,11 @@ public class Login extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+
+    public void inscriptionActivity(View view)
+    {
+        Intent i = new Intent(Login.this,InscriptionActivity.class);
+        startActivity(i);
     }
 }
